23f38eaeac272ebd74d52bc02211e9c45f5652de
dollweb <101566500+dollweb@users.noreply.github.com>
2024-08-26 19:48:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -133,3 +133,6 @@ st.markdown("""
     <iframe src="https://coupa.ng/cf3Jkx" width="100%" height="75" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
 </div>
 """, unsafe_allow_html=True)
+
+# 쿠팡 파트너스 안내 문구 추가
+st.markdown("<p style='text-align: center; font-size: 0.8em;'>* 쿠팡 파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있습니다.</p>", unsafe_allow_html=True)
