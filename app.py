264fcdf83c1884421adab2a47416c32a3bd78d7d
dollweb <101566500+dollweb@users.noreply.github.com>
2024-08-26 19:55:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 쿠팡 파트너스 안내 문구 추가
-st.markdown("<p style='text-align: center; font-size: 0.8em;'>* 쿠팡 파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있습니다.</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 0.8em;'>※ 쿠팡 파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있습니다.</p>", unsafe_allow_html=True)
