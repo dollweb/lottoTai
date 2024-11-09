4f3ce3603eb702351248a05fa57c6cd993987197
dollweb <101566500+dollweb@users.noreply.github.com>
2024-11-09 19:19:50 +0900

    Add files via upload

--- pages/2_chatbot.py
+++ pages/2_chatbot.py
@@ -2,7 +2,8 @@ import streamlit as st
 import openai
 
 # OpenAI API 키 설정 (환경 변수에서 불러오는 것이 좋음)
-openai.api_key = "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"  # 여기에 API 키를 입력하세요.
+API_KEY = '{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}'
+openai.api_key = API_KEY
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "무엇을 알려드릴까요?"}]
