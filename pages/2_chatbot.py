7366ea3d6ed19122b04acfc35bc46b0bf934bc5d
dollweb <101566500+dollweb@users.noreply.github.com>
2024-11-23 20:44:48 +0900

    Add files via upload

--- pages/2_chatbot.py
+++ pages/2_chatbot.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import openai
+from openai import OpenAI
 
 st.title("lottoTai")
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
